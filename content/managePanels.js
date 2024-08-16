f9d8096cdd6796e45783f79722d5cace93e6d8b3
qooxx <bernardopvsouza@gmail.com>
2024-08-16 14:26:24 -0300

    feat: popup that enables control over audio and player display

---
+++ content/managePanels.js
@@ -0,0 +1,66 @@
+const radioPanel = document.querySelector("#area_player")
+const radioPlayer = document.querySelector("#player2")
+
+function removeIframes() {
+	const iframes = document.querySelectorAll('iframe')
+	if (iframes) {
+		iframes.forEach(iframe => {
+			if (iframe.src.includes('google.com') || iframe.src.includes('doubleclick.net')) {
+				iframe.remove();
+			}
+		});
+	}
+}
+
+function removeGoogleAdPanels() {
+	const googlePanels = document.querySelectorAll('.adsbygoogle, .google_ad, [id^="google_ads"], [class*="adsbygoogle"]')
+	if (googlePanels) {
+		googlePanels.forEach(ad => {
+			ad.remove();
+		});
+	}
+}
+
+function handlePanelToggle(radioPanelStatus) {
+	const value = radioPanelStatus ? 'hidden' : ''
+	radioPanel.setAttribute('style', `content-visibility: ${value};`)
+}
+
+function handlePlayerToggle(radioPlayerStatus) {
+	if (radioPlayerStatus) {
+		radioPlayer.removeAttribute("autoplay")
+		radioPlayer.pause()
+	} else {
+		radioPlayer.play()
+	}
+}
+
+async function handleInitialValues() {
+	const result = await chrome.storage.sync.get(["radioPanelStatus", "radioPlayerStatus"])
+	const parsedStatus = JSON.parse(`{"radioPanelStatus": ${result.radioPanelStatus}, "radioPlayerStatus": ${result.radioPlayerStatus}}`)
+
+	const { radioPanelStatus, radioPlayerStatus } = parsedStatus
+	handlePanelToggle(radioPanelStatus)
+	handlePlayerToggle(radioPlayerStatus)
+
+}
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+	if (request.action === "first-load-or-refresh") {
+		removeIframes()
+		removeGoogleAdPanels()
+		handleInitialValues()
+
+		sendResponse({ status: "Executado" })
+	}
+
+	else if (request.action === "hide-radio-panel") {
+		const { radioPanelStatus } = request
+		handlePanelToggle(radioPanelStatus)
+	}
+
+	else if (request.action === "radio-player-click") {
+		const { radioPlayerStatus } = request
+		handlePlayerToggle(radioPlayerStatus)
+	}
+})

a2a6486e069c13a380987b67446ae7cef27e0363
qooxx <bernardopvsouza@gmail.com>
2025-02-14 12:33:55 -0300

    chore: removing dist folder

--- dist/utils/removeDomElements.js
+++
@@ -1,1 +0,0 @@
-(()=>{function a(o){o&&o.forEach(f=>{f&&f.forEach(r=>{r.remove()})})}var c=a;})();
--- dist/utils/storage.js
+++
@@ -1,1 +0,0 @@
-(()=>{async function a(e){let t=await chrome.storage.sync.get([e]);return JSON.parse(t[e])}})();
